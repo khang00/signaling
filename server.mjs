50e29d9a3bcc2b672a2d6ae60bdf4dfe5f1f4e70
Khang Đặng (Software Engineer Intern) <khang.dang@be.com.vn>
2021-01-10 13:50:24 +0700

    update signaling server

--- server.mjs
+++ server.mjs
@@ -1,36 +1,44 @@
-import {Server} from 'socket.io'
+import Peer from 'simple-peer'
+import io from 'socket.io-client'
+import wrtc from 'wrtc'
+import { Server } from 'socket.io'
 
 const CORS_OPTIONS = {
-    'Access-Control-Allow-Origin': '*'
+	'Access-Control-Allow-Origin': '*'
 }
-const CONNECTED_USERS = new Map()
 
-const server = new Server(8000, {
-    path: '/',
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false,
-    cors: CORS_OPTIONS
+const server = new Server(3000, {
+	path: '/',
+	pingInterval: 10000,
+	pingTimeout: 5000,
+	cookie: false,
+	cors: CORS_OPTIONS
 })
+
+const LOGINED_USERS = new Map()
 
 server.on('connection', socket => {
-    socket.emit('connected')
+	socket.emit('connection', '')
+	socket.on('login', username => {
+		LOGINED_USERS.set(username, socket.id)
+		console.log(`${username} logined`)
+	})
 
-    socket.on('login', data => {
-        CONNECTED_USERS.set(data.username, socket.id)
-    })
+	socket.on('signal', data => {
+		server.to(LOGINED_USERS.get(data.to)).emit('signal', data)
+	})
 
-    socket.on('message', data => {
-        if (CONNECTED_USERS.has(data.recipient)) {
-            const recipient = CONNECTED_USERS.get(data.recipient)
-            console.log(data.content)
-            server.to(recipient).emit('message', data)
-        }
-    })
+	socket.on('initiate', data => {
+		server.to(LOGINED_USERS.get(data.to)).emit('initiate', data)
+	})
 
-    socket.on('disconnecting', reason => {
-        console.log(reason)
-    })
+	socket.on('disconnecting', reason => {
+		console.log(reason)
+		for (let [username, socketId] of LOGINED_USERS) {
+			if (socketId === socket.id) {
+				LOGINED_USERS.delete(username)
+				return
+			}
+		}
+	})
 })
-
-server.on('error', () => console.log('Socket error'))
